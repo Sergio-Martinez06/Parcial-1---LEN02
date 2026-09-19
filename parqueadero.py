b927e8ac29c3bba84d3b596fc61e58373184e11d
Sergio-Martinez06 <sergioadrianmartinez10@gmail.com>
2026-09-18 19:43:38 -0500

    Implementar contadores
    feat implementar contandores para vehiculos y diferentes tipos de atributos

--- parqueadero.py
+++ parqueadero.py
@@ -7,3 +7,12 @@ es_sabado = sabado_texto == "True" or sabado_texto == "true"
 if type(sabado_texto) != bool:
     print("ERROR: valor de 'es_sabado' inválido. Se asumirá que no es sábado.")
     es_sabado = False
+vehiculos_registrados = 0
+total_recaudado = 0.0
+total_estudiantes = 0
+total_docentes = 0
+total_visitantes = 0
+suma_horas = 0.0
+
+contador = 0
+
